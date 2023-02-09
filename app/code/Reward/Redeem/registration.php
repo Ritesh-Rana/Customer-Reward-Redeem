ef99245436bfbff6f71bec6a86cff7ffaa8bd89a
Ritesh Rana <13ritesh2001@gmail.com>
2023-02-09 11:59:10 +0530

    adding module structure

---
+++ app/code/Reward/Redeem/registration.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * @package        Ritesh_Rana
+ * @author         Ritesh Rana
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Reward_Redeem',
+    __DIR__
+);
