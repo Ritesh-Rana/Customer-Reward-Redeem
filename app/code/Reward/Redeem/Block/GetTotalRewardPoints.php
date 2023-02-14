c38be48ebf44d6a818fb8890224a3e90d4533696
Ritesh Rana <13ritesh2001@gmail.com>
2023-02-14 19:03:49 +0530

    adding Block folder

---
+++ app/code/Reward/Redeem/Block/GetTotalRewardPoints.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Reward\Redeem\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class GetTotalRewardPoints extends Template {
+
+    public function GetCustomerTotalRewardPoints(){
+
+    }
+}
